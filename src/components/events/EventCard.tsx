7acb143a61fdbdd501483a638343c34e71a074c2
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-09 15:29:37 -0500

    more card stuff

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -8,27 +8,27 @@ interface EventCardProps {
 const EventCard = ({ event }: EventCardProps) => {
   return (
     <>
-      <div className="max-w-sm overflow-hidden">
-        <div className="max-w-[432px]  mb-[24px] px-[20px] mt-[52px]">
+      <div className="group max-w-sm overflow-hidden rounded-[20px] transition ease-in-out hover:bg-[#F2F4F8]">
+        <div className="mt-[52px] mb-[24px] px-[20px] max-w-[432px]">
           <img
-            className="w-full rounded-[20px] h-[288px] object-cover"
+            className="w-full h-[288px] object-cover rounded-[20px]"
             src={`/img/${event.id}.jpg`}
             alt={`${event.title}`}
           />
         </div>
         <div className="px-[20px]">
-          <div className="font-[200] text-[24px] mb-[12px] leading-7">
+          <div className="mb-[12px] text-[19px] font-[200] leading-7 transition ease-in-out group-hover:font-[700]">
             {event.title}
           </div>
-          <div className="font-[200] text-[16px] text-[#474C58] mb-[8px]">
+          <div className="mb-[8px] text-[16px] font-[200] text-[#474C58] transition ease-in-out group-hover:font-[700]">
             {formatDate(event.dateTime)}
           </div>
-          <div className="text-[16px] text-[#999FAA] mb-[20px]">
+          <div className="mb-[20px] text-[16px] text-[#999FAA]">
             {event.venue}
           </div>
         </div>
         <div className="mb-[15px]">
-          <span className="inline-block rounded-[10px] bg-[#F2F4F8] ml-[7px] px-[12px] py-[10px] text-sm font-[200] text-[#474C58] text-[16px]">
+          <span className="ml-[7px] inline-block rounded-[10px] bg-[#F2F4F8] px-[12px] py-[10px] text-sm text-[16px] font-[200] text-[#474C58] transition ease-in-out group-hover:font-[700]">
             View more info
           </span>
         </div>
