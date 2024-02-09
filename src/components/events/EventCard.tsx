9446f1cdd757d0c5c11299d0d1fce9e706f11beb
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-09 02:28:12 -0500

    organized classes

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,30 +1,30 @@
-import React from "react";
 import { Event } from "../../common/types";
 import { formatDate } from "../../common/helpers";
 
 interface EventCardProps {
   event: Event;
 }
+
 const EventCard = ({ event }: EventCardProps) => {
   return (
     <>
       <div className="max-w-sm overflow-hidden">
         <img
-          className="ml-[9px] mb-[24px] w-full rounded-lg"
+          className="w-full rounded-lg mb-[24px] ml-[9px]"
           src={`/img/${event.id}.jpg`}
           alt={`${event.title}`}
         />
         <div className="ml-[9px]">
-          <div className="text-[24px] mb-[12px] font-[200]">{event.title}</div>
-          <div className="text-[16px] mb-[4px] font-[200] text-[#474C58]">
+          <div className="font-[200] text-[24px] mb-[12px]">{event.title}</div>
+          <div className="font-[200] text-[16px] text-[#474C58] mb-[4px]">
             {formatDate(event.dateTime)}
           </div>
-          <div className="text-[16px] mb-[20px] text-[#999FAA]">
+          <div className="text-[16px] text-[#999FAA] mb-[20px]">
             {event.venue}
           </div>
         </div>
-        <div className="">
-          <span className="inline-block bg-[#F2F4F8] rounded-[10px] px-[10px] py-[10px] text-sm font-[200] text-gray-700 mr-2 mb-2 text-[16px]">
+        <div>
+          <span className="inline-block rounded-[10px] bg-[#F2F4F8] px-[10px] py-[10px] mr-2 mb-2 text-sm font-[200] text-gray-700 text-[16px]">
             View more info
           </span>
         </div>
