b1d8d2ba957acead5c17c5729cd2aa59065c643a
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-09 01:10:09 -0500

    font

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -14,9 +14,11 @@ const EventCard = ({ event }: EventCardProps) => {
           alt={`${event.title}`}
         />
         <div className="pt-4">
-          <div className="text-xl mb-2">{event.title}</div>
-          <div className="text-xl">{event.dateTime}</div>
-          <div className="text-sm mb-[24px] text-[#999FAA]">{event.venue}</div>
+          <div className="text-[30px] mb-2">{event.title}</div>
+          <div className="text-[20px]">{event.dateTime}</div>
+          <div className="text-[20px] mb-[24px] text-[#999FAA]">
+            {event.venue}
+          </div>
         </div>
         <div className="pb-2">
           <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
