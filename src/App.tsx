4060c7826ffc0fc9faa2898c0c6ff464dfcdaa70
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-08 22:58:53 -0500

    images added + connection to supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useSupabaseClient } from "./lib/SupabaseProvider";
 
-console.log(process.env.SUPABASE_URL);
+export interface Event {
+  id: number;
+  title: string;
+  dateTime: string;
+  venue: string;
+  link: string;
+  imgLink: string;
+}
+
 function App() {
-  return <>Hello</>;
+  const supabase = useSupabaseClient();
+  const [events, setEvents] = useState<Event[] | null>(null);
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  async function getEvents() {
+    const { data } = await supabase.from("Events").select();
+    setEvents(data);
+  }
+
+  return (
+    <ul>
+      {events?.map((event) => (
+        <>
+          <li key={event.id}>{event.title}</li>
+          <img src={`/img/${event.id}.jpg`} />
+        </>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
