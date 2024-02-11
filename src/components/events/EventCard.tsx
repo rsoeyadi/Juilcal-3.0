6ceac25f596afe60aacecd9c7471c377f6b4eb49
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-10 22:11:05 -0500

    mobile

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -21,19 +21,19 @@ const EventCard = ({ event }: EventCardProps) => {
 
   return (
     <>
-      <div className="group m-auto md:w-[472px] max-w-md overflow-hidden rounded-[20px] transition ease-in-out hover:bg-[#F2F4F8]">
-        <div className="px-[20px] mt-[52px] mb-[24px]">
+      <div className="group max-w-md m-auto overflow-hidden rounded-[20px] transition ease-in-out md:hover:bg-[#F2F4F8] md:w-[472px]">
+        <div className="mb-[24px] mt-[52px] px-[20px]">
           <img
-            className="w-full h-[288px] object-cover rounded-[20px]"
-            src={`/img/${event.id}.jpg`}
             alt={event.title}
+            className="h-[288px] object-cover rounded-[20px] w-full"
+            src={`/img/${event.id}.jpg`}
           />
         </div>
         <div className={`px-[20px] ${getHeightClass()}`}>
-          <div className="mb-[12px] text-[23px] font-[200] leading-7 transition ease-in-out group-hover:font-[600]">
+          <div className="font-[600] mb-[12px] md:font-[200] md:group-hover:font-[600] text-[22px] leading-7 transition ease-in-out">
             {event.title}
           </div>
-          <div className="mt-3 mb-[8px] text-[16px] font-[200] text-[#474C58] transition ease-in-out group-hover:font-[600]">
+          <div className="font-[600] mt-3 mb-[8px] md:font-[200] md:group-hover:font-[600] text-[16px] text-[#474C58] transition ease-in-out">
             {formatDate(event.dateTime)}
           </div>
           <div className="mb-[20px] text-[16px] text-[#999FAA]">
@@ -41,7 +41,7 @@ const EventCard = ({ event }: EventCardProps) => {
           </div>
         </div>
         <div className="mb-[15px]">
-          <span className="inline-block ml-[7px] rounded-[10px] bg-[#F2F4F8] px-[12px] py-[10px] text-sm text-[16px] font-[200] text-[#474C58] transition ease-in-out group-hover:font-[600]">
+          <span className="bg-[#F2F4F8] font-[600] inline-block ml-[7px] md:font-[200] md:group-hover:font-[600] px-[12px] py-[10px] rounded-[10px] text-[16px] text-[#474C58] text-sm transition ease-in-out">
             View more info
           </span>
         </div>
