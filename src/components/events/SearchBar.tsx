be148186319ea7526bbdbdc578549987476fa6b3
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-13 00:04:20 -0500

    search bar

--- src/components/events/SearchBar.tsx
+++ src/components/events/SearchBar.tsx
@@ -3,23 +3,25 @@ import FiltersIcon from "/filter_list.svg";
 
 const SearchBar = () => {
   return (
-    <form>
-      <div className="relative min-w-full">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <img className="w-4 h-4 text-gray-500" src={SearchIcon} />
+    <div className="mx-11">
+      <form>
+        <div className="relative min-w-full">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <img className="w-4 h-4 text-gray-500" src={SearchIcon} />
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-[12px] bg-[#F2F4F8] focus:ring-blue-500 focus:border-blue-500 "
+            placeholder="Search for a Juilliard event"
+            required
+          />
+          <div className="absolute inset-y-0 end-5 flex items-center ps-3 pointer-events-none">
+            <img className="w-4 h-4 text-gray-500" src={FiltersIcon} />
+          </div>
         </div>
-        <input
-          type="search"
-          id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-[#F2F4F8] focus:ring-blue-500 focus:border-blue-500 "
-          placeholder="Search for a Juilliard event"
-          required
-        />
-        <div className="absolute inset-y-0 end-5 flex items-center ps-3 pointer-events-none">
-          <img className="w-4 h-4 text-gray-500" src={FiltersIcon} />
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
