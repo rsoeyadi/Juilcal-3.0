a7034a63cde6193ed59bb85cefb6a4870acd4403
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-13 00:05:37 -0500

    search bar

--- src/components/events/SearchBar.tsx
+++ src/components/events/SearchBar.tsx
@@ -6,18 +6,18 @@ const SearchBar = () => {
     <div className="mx-11">
       <form>
         <div className="relative min-w-full">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <img className="w-4 h-4 text-gray-500" src={SearchIcon} />
+          <div className="absolute inset-y-0 start-0 flex items-center pl-3 pointer-events-none">
+            <img src={SearchIcon} className="w-4 h-4 text-gray-500" />
           </div>
           <input
             type="search"
             id="default-search"
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-[12px] bg-[#F2F4F8] focus:ring-blue-500 focus:border-blue-500 "
+            className="block w-full px-4 pl-10 py-4 text-sm text-gray-900 bg-[#F2F4F8] border border-gray-300 rounded-[12px] focus:border-blue-500 focus:ring-blue-500"
             placeholder="Search for a Juilliard event"
             required
           />
-          <div className="absolute inset-y-0 end-5 flex items-center ps-3 pointer-events-none">
-            <img className="w-4 h-4 text-gray-500" src={FiltersIcon} />
+          <div className="absolute inset-y-0 end-0 flex items-center pr-5 pointer-events-none">
+            <img src={FiltersIcon} className="w-4 h-4 text-gray-500" />
           </div>
         </div>
       </form>
