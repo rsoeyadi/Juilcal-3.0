ef621e6e74fd916459d3db7ce06a2e2ff8c02431
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-25 23:35:55 -0500

    added slice for bookmarked events

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { searchSlice } from "./store/search/searchSlice";
+import { bookedmarkedEventsSlice } from "./store/BookmarkedEvents/bookmarkedEvents";
 
 export default configureStore({
   reducer: {
     search: searchSlice.reducer,
+    bookmarkedEvents: bookedmarkedEventsSlice.reducer,
   },
 });
