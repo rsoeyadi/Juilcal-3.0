15e79209ac6dad7f3bccfcac6bcf193ede3b3d25
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-02-28 21:42:04 -0500

    import removed

--- src/store/BookmarkedEvents/bookmarkedEventsSlice.ts
+++ src/store/BookmarkedEvents/bookmarkedEventsSlice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { Event as MyEvent } from "../../common/types";
 
 export interface BookmarkedEventsState {
